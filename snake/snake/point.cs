f04700a3e3767bbf827cb4f024099deca922321e
Aleks <alcidelnkob1982@yandex.ru>
2015-10-24 17:24:57 +0300

    Bilder

--- snake/snake/point.cs
+++ snake/snake/point.cs
@@ -12,6 +12,14 @@ namespace snake
         public int y;
         public char sym;
 
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+
+        }
+
         public void Draw()
         {
 
